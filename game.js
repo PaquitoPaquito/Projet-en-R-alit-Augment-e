c6ff8d70419b55d7502e83d5e8962f86c598afce
bobinnard <skyland1360@gmail.com>
2026-04-21 15:32:03 +0200

    finished implementing a turn

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 class game{
     greniers = [ , , , , , , , , , , , ];
     current_player = 0;
+    scores = [0,0];
 
     constructor(){
         for (i in Range(0,11)){
@@ -12,8 +13,15 @@ class game{
     play(grenierId) {
         var grenierFinal = 0;
         for (i in Range(1,greniers[grenierId].nb_graines)){
-            greniers[grenierId+i].nb_graines++;
-            grenierFinal = grenierId+i;
+            greniers[(grenierId+i)%12].nb_graines++;
+            grenierFinal = (grenierId+i)%12;
         }
+        var currentGrenier = this.greniers[grenierFinal];
+        while (0 < currentGrenier.nb_graines < 3 && currentGrenier.player_id != this.current_player){
+            this.scores[this.current_player] += currentGrenier.nb_graines;
+            currentGrenier.nb_graines = 0;
+            currentGrenier = this.greniers[(currentGrenier.id-1)%12];
+        }
+        this.current_player = 1-this.current_player;
     }
 }
